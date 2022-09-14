fa07625ce7e22eaa9f1c75900b08fefc666bfcab
jlit2594 <jdlitton2594@gmail.com>
2022-09-13 20:46:39 -0500

    fixed some positioning

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -3,9 +3,9 @@ import Nav from 'react-bootstrap/Nav';
 
 function Navigate() {
   return (
-    <Nav class='navbar' variant="tabs" defaultActiveKey="/about">
+    <Nav class='navbar' variant="tabs" defaultActiveKey="#about">
       <Nav.Item className='nav-item'>
-        <Nav.Link className='no-line'href="/about">About</Nav.Link>
+        <Nav.Link className='no-line'href="#about">About</Nav.Link>
       </Nav.Item>
       <Nav.Item className='nav-item'>
         <Nav.Link className='no-line' eventKey="/projects">Projects</Nav.Link>
